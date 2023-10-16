e0dcf6c2c1e930d93a72541af22d8a131ec021ec
Jayabrata Pramanik <jayabrata180402@gmail.com>
2023-10-17 01:24:35 +0530

    test

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,6 +1,19 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
+import request from "supertest";
 import { app } from "../app";
+
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signin(): Promise<string[]>;
+    }
+  }
+}
+
+
+
 
 let mongo: any;
 
@@ -26,3 +39,21 @@ afterAll(async () => {
   }
   await mongoose.connection.close();
 });
+
+// global.signin = async () => {
+//   const email = "test@test.com";
+//   const password = "password";
+
+//   const response = await request(app)
+//     .post("/api/users/signup")
+//     .send({
+//       email,
+//       password,
+//     })
+//     .expect(201);
+
+//   const cookie = response.get("Set-Cookie");
+
+//   return cookie;
+// };
+
